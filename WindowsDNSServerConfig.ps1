c5b1e4fec420b6514b4ae6d88a7821e25719f27c
Michael Greene <michael.greene@microsoft.com>
2018-02-23 05:59:18 -0600

    remove param()

--- WindowsDNSServerConfig.ps1
+++ WindowsDNSServerConfig.ps1
@@ -40,7 +40,6 @@ https://github.com/Microsoft/WindowsDNSServerConfig/blob/master/README.md##relea
  This module contains PowerShell Desired State Configuration solutions for deploying and configuring DNS Servers 
 
 #> 
-Param()
 
 configuration WindowsDNSServerConfig
 {
@@ -58,78 +57,76 @@ configuration WindowsDNSServerConfig
     Import-DscResource -module 'xDnsServer','PSDesiredStateConfiguration'
     
     $ZoneData = 
+        @{
+            PrimaryZone     = 'Contoso.com';
+            ARecords        = 
             @{
-                PrimaryZone     = 'Contoso.com';
-                ARecords        = 
-                @{
-                    'ARecord1'  = '10.0.0.25';
-                    'ARecord2'  = '10.0.0.26';
-                    'ARecord3'  = '10.0.0.27'
-                };
-                CNameRecords    = 
-                @{
-                    'www'       = 'ARecord1';
-                    'wwwtest'   = 'ARecord2';
-                    'wwwqa'     = 'ARecord3'
-                }
-            },
+                'ARecord1'  = '10.0.0.25';
+                'ARecord2'  = '10.0.0.26';
+                'ARecord3'  = '10.0.0.27'
+            };
+            CNameRecords    = 
             @{
-                PrimaryZone     = 'Fabrikam.com';
-                ARecords        = 
-                @{
-                    'ARecord1'  = '10.0.0.35';
-                    'ARecord2'  = '10.0.0.36';
-                    'ARecord3'  = '10.0.0.37'
-                };
-                CNameRecords    = 
-                @{
-                    'www'       = 'ARecord1';
-                    'wwwtest'   = 'ARecord2';
-                    'wwwqa'     = 'ARecord3'
-                }
+                'www'       = 'ARecord1';
+                'wwwtest'   = 'ARecord2';
+                'wwwqa'     = 'ARecord3'
             }
-
-    {
-        WindowsOptionalFeature DNS
-        {
-            Ensure  = 'Enable'
-            Name    = 'DNS-Server-Full-Role'
+        },
+        @{
+            PrimaryZone     = 'Fabrikam.com';
+            ARecords        = 
+            @{
+                'ARecord1'  = '10.0.0.35';
+                'ARecord2'  = '10.0.0.36';
+                'ARecord3'  = '10.0.0.37'
+            };
+            CNameRecords    = 
+            @{
+                'www'       = 'ARecord1';
+                'wwwtest'   = 'ARecord2';
+                'wwwqa'     = 'ARecord3'
+            }
         }
-        
-        foreach ($Zone in $ZoneData)
+
+    WindowsOptionalFeature DNS
+    {
+        Ensure  = 'Enable'
+        Name    = 'DNS-Server-Full-Role'
+    }
+    
+    foreach ($Zone in $ZoneData)
+    {
+        xDnsServerPrimaryZone $Zone.PrimaryZone
         {
-            xDnsServerPrimaryZone $Zone.PrimaryZone
-            {
-                Ensure    = 'Present'                
-                Name      = $Zone.PrimaryZone
-                DependsOn = '[WindowsOptionalFeature]DNS'
-            }
+            Ensure    = 'Present'                
+            Name      = $Zone.PrimaryZone
+            DependsOn = '[WindowsOptionalFeature]DNS'
+        }
 
-            foreach ($ARecord in $Zone.ARecords.Keys)
+        foreach ($ARecord in $Zone.ARecords.Keys)
+        {
+            xDnsRecord "$($Zone.PrimaryZone)_$ARecord"
             {
-                xDnsRecord "$($Zone.PrimaryZone)_$ARecord"
-                {
-                    Ensure    = 'Present'
-                    Name      = $ARecord
-                    Zone      = $Zone.PrimaryZone
-                    Type      = 'ARecord'
-                    Target    = $Zone.ARecords[$ARecord]
-                    DependsOn = "[WindowsOptionalFeature]DNS","[xDnsServerPrimaryZone]$($Zone.PrimaryZone)"
-                }        
-            }
+                Ensure    = 'Present'
+                Name      = $ARecord
+                Zone      = $Zone.PrimaryZone
+                Type      = 'ARecord'
+                Target    = $Zone.ARecords[$ARecord]
+                DependsOn = "[WindowsOptionalFeature]DNS","[xDnsServerPrimaryZone]$($Zone.PrimaryZone)"
+            }        
+        }
 
-            foreach ($CNameRecord in $Zone.CNameRecords.Keys)
+        foreach ($CNameRecord in $Zone.CNameRecords.Keys)
+        {
+            xDnsRecord "$($Zone.PrimaryZone)_$CNameRecord"
             {
-                xDnsRecord "$($Zone.PrimaryZone)_$CNameRecord"
-                {
-                    Ensure    = 'Present'
-                    Name      = $CNameRecord
-                    Zone      = $Zone.PrimaryZone
-                    Type      = 'CName'
-                    Target    = $Zone.CNameRecords[$CNameRecord]
-                    DependsOn = "[WindowsOptionalFeature]DNS","[xDnsServerPrimaryZone]$($Zone.PrimaryZone)"
-                }        
-            }
+                Ensure    = 'Present'
+                Name      = $CNameRecord
+                Zone      = $Zone.PrimaryZone
+                Type      = 'CName'
+                Target    = $Zone.CNameRecords[$CNameRecord]
+                DependsOn = "[WindowsOptionalFeature]DNS","[xDnsServerPrimaryZone]$($Zone.PrimaryZone)"
+            }        
         }
     }
 }
